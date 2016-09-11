2afedf60c77c680d43a42f05831fe31eafee5792
Marcin Marczyk <m.l.marczyk@gmail.com>
2016-09-11 17:31:21 +0200

    check

--- routes/api.js
+++ routes/api.js
@@ -18,8 +18,9 @@ router.get('/temperature', function(req, res){
     ds18b20.sensors(function(err, ids){
       console.log(ids[0]);
       id = ids[0];
+      temperature.celsius = ds18b20.temperatureSync(id);
+      console.log(temperature);
     });
-    temperature.celsius = ds18b20.temperatureSync(id);
   }
 
   res.json(temperature);
