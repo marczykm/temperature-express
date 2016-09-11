1b06ce32ffe1f95da2c17c6c6bd4e69038fd3eb7
Marcin Marczyk <m.l.marczyk@gmail.com>
2016-09-11 18:18:40 +0200

    getting data from server

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/angular', express.static(path.join(__dirname, 'node_modules/angular')));
+app.use('/angular-resource', express.static(path.join(__dirname, 'node_modules/angular-resource')));
+
 
 app.use('/', routes);
 app.use('/api', api);
